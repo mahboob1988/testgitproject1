d3d0b27e90fcb6720ada26bcce72d96a107a73bb
Mobashir <mahboobmobashir88@gmail.com>
2023-08-16 13:46:29 +0530

    Create testgitproject1

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Mahboob
+ *
+ */
+module testgitproject1 {
+}
---
+++ src/testpackage/WelcomeTest.java
@@ -0,0 +1,10 @@
+package testpackage;
+
+public class WelcomeTest {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome in Oracle Robotics AI");
+
+	}
+
+}
